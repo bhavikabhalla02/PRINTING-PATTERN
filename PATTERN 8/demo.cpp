e1ca120ec5d2d2d8186496f03bfd4a2e1a05b92e
bhavikabhalla02 <bhavika3614.be20@chitkara.edu.in>
2022-07-16 21:42:16 +0530

    Update demo.cpp

--- PATTERN 8/demo.cpp
+++ PATTERN 8/demo.cpp
@@ -24,14 +24,14 @@ int main()
         }
         cout<<endl;
     } 
-    // int n;
-    // cin>>n;
-    // for(int i=1;i<=n;i++){
-    //     for(int j=1;j<=i;j++){
-    //         cout<<j<<" ";
-    //     }
-    //     cout<<endl;
-    // }   
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }   
         
  return 0;
 }
